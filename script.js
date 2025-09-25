9d59229f65065b19055458f6303a2e26e6a485d2
Maple <135763163+maple-johnson@users.noreply.github.com>
2025-09-25 12:54:09 -0700

    Set up basic upgrade functionality.

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
+let upOne = false;
+
 window.onload = function() {
     document.getElementById("button").addEventListener("click", scoreUp);
     document.getElementById("button").addEventListener("click", achievementChecker);
+    this.document.getElementById("multi_upgrade").addEventListener("click", upgradeOne);
 }
 
 function scoreUp() {
     let originalScore = parseInt(document.getElementById("score").innerHTML);
-    let newScore = originalScore + 1;
+    let modifier = 0;
+
+    if (!upOne) modifier = 1;
+    else modifier = 5
+
+    let newScore = originalScore + modifier;
+
     document.getElementById("score").innerHTML = newScore;
 }
 
 function achievementChecker() {
     let totalClicks = parseInt(document.getElementById("score").innerHTML);
     if (totalClicks === 10) alert("10 Clicks!");
+}
+
+function upgradeOne() {
+    upOne = true;
 }
